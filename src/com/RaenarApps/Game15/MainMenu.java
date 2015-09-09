479e56554d85877a728fe97027ad28fe05b22f77
Raenar4k <RaenarDev@gmail.com>
2015-09-09 16:43:19 +0200

    Now new game wont start if image is not selected

--- src/com/RaenarApps/Game15/MainMenu.java
+++ src/com/RaenarApps/Game15/MainMenu.java
@@ -65,19 +65,14 @@ public class MainMenu extends Activity {
                     isDefault = data.getBooleanExtra(Image.IS_DEFAULT, false);
                     isProcessed = data.getBooleanExtra(Image.IS_PROCESSED, false);
                     dominantColor = data.getStringExtra(Image.DOMINANT_COLOR);
-                } else {
-                    imagePath = "backgrounds/pollen.jpg";
-                    isDefault = true;
-                    isProcessed = false;
-                    dominantColor = null;
+                    intent.putExtra(Image.IMAGE_PATH, imagePath);
+                    intent.putExtra(Image.IS_DEFAULT, isDefault);
+                    intent.putExtra(Image.IS_PROCESSED, isProcessed);
+                    intent.putExtra(Image.DOMINANT_COLOR, dominantColor);
+                    intent.putExtra(FifteenActivity.IS_NEW_GAME, true);
+                    setResult(RESULT_OK, intent);
+                    startActivityForResult(intent, REQUEST_UPDATE);
                 }
-                intent.putExtra(Image.IMAGE_PATH, imagePath);
-                intent.putExtra(Image.IS_DEFAULT, isDefault);
-                intent.putExtra(Image.IS_PROCESSED, isProcessed);
-                intent.putExtra(Image.DOMINANT_COLOR, dominantColor);
-                intent.putExtra(FifteenActivity.IS_NEW_GAME, true);
-                setResult(RESULT_OK, intent);
-                startActivityForResult(intent, REQUEST_UPDATE);
                 break;
             case REQUEST_UPDATE:
                 updateMenu();
